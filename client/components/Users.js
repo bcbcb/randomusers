11e4a522c8df39635319535ff3291010bbf5b7ab
bcbcb <bcbcb@users.noreply.github.com>
2017-06-04 16:45:17 -0700

    list by letter

--- client/components/Users.js
+++ client/components/Users.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import User from './User'
 
-const Users = ({ users }) => (
-  <ul className="list">
-    {users.map(user => <User {...user} key={user.login.salt} />)}
-  </ul>
-);
+import styles from './Users.css'
+
+const Users = ({ users }) => {
+  const letters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  const usersByLetter = letters.map(
+    letter => {
+      return {
+        letter: letter,
+        users: users.filter(user => (user.name.first.charAt(0) === letter))
+      }
+    }
+  )
+
+  return (
+    <div>
+      {usersByLetter.map(letter => (
+        letter.users.length > 0 &&
+        <div>
+          <div className={styles.letter}>
+            {letter.letter.toUpperCase()}
+          </div>
+          <ul className={styles.list}>
+            {letter.users.map(user => <User {...user} key={user.login.salt} />)}
+          </ul>
+        </div>
+        )
+      )}
+    </div>
+  )
+}
 
 export default Users
